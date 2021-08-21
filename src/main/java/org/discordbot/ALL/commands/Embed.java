50b2dc8153a42141b87170ef8316e3b58c7a6e7a
INarveyL <79750868+INarveyL@users.noreply.github.com>
2021-08-21 18:20:37 +0500

    Added Embed to the embed file

--- src/main/java/org/discordbot/ALL/commands/Embed.java
+++ src/main/java/org/discordbot/ALL/commands/Embed.java
@@ -2,12 +2,26 @@ package org.discordbot.ALL.commands;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.awt.*;
 
 public class Embed implements CommandExecutor {
 
-    @Command(aliases = "Ping", description = "I dont know")
-    public String onPingCommand()
+    @Command(aliases = "embed", description = "I dont know")
+    public void onPingCommand(MessageCreateEvent message)
     {
-        return "hello test 12345 does new bot work";
+        message.getChannel().sendMessage(embed(message));
+    }
+
+    private EmbedBuilder embed(MessageCreateEvent message)
+    {
+        return new EmbedBuilder()
+                .setTitle("TEST")
+                .setDescription("This is an embed")
+                .setColor(new Color(99,123,121))
+                .setFooter("Hello", message.getMessageAuthor().getAvatar());
     }
 }
